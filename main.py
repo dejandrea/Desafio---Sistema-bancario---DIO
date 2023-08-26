18f79291073bfe587a67b72949b6630cd17c7b35
dejandrea <dejandrea@gmail.com>
2023-08-26 20:41:36 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+menu = '''
+Escolha uma opcao:
+
+[d] = Deposito
+[s] = Saque
+[e] = Extrato
+[q] = Sair
+
+===> 
+'''
+
+saldo = 0
+limite = 500
+num_saques = 0
+LIMITE_SAQUES = 3
+extrato = ""
+
+while True:
+    opcao = input(menu)
+
+    if opcao == "d":
+        pass
+    elif opcao == "s":
+        pass
+    elif opcao == "e":
+        pass
+    elif opcao == "q":
+        break
+    else:
+        pass
